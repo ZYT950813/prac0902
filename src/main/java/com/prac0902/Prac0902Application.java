fe638559eefc74bce75fb52882e742444c11b5f5
git-test <tao-email>
2022-09-02 17:04:52 +0800

    first commit create projiect

---
+++ src/main/java/com/prac0902/Prac0902Application.java
@@ -0,0 +1,13 @@
+package com.prac0902;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prac0902Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prac0902Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/prac0902/Prac0902ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prac0902;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prac0902ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
